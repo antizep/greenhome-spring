66a7d47dde2662d62c9ccd19ffa890c944d76b08
antizep <antizep@mail.u>
2026-06-20 17:15:01 +0000

    feature: Настроен гитигнор, и создан нустой spring boot проект

---
+++ src/main/java/ru/antizep/greenhouse/spring/GreenhouseSpring.java
@@ -0,0 +1,13 @@
+package ru.antizep.greenhouse.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenhouseSpring {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenhouseSpring.class, args);
+	}
+
+}
