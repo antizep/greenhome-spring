38e447d61dd28f9926dbd61e68eac2a7299df6fe
antizep <antizep@mail.u>
2026-06-22 20:47:46 +0300

    refactor: переделать spring-boot hello world с использованием gradle wrapper

---
+++ lib/src/main/java/ru/antizep/greenhome/spring/GrenhomeSpring.java
@@ -0,0 +1,13 @@
+package ru.antizep.greenhome.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrenhomeSpring {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrenhomeSpring.class, args);
+	}
+
+}
